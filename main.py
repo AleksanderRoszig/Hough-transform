5b28b192dc1f9030b76ac988ef610692ad3eea27
Aleksander <aleksrosz@gmail.com>
2021-04-27 20:45:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,9 +22,34 @@ hough_space = np.zeros((r_dim,theta_dim))
 
 for x in range(x_max):
     for y in range(y_max):
-        if image_data[x,y] == 1:
+        if image_data[x, y, 0] == 0:
             for itheta in range(theta_dim):
                 theta = 1 * itheta * theta_max / theta_dim
                 r = x * math.cos(theta) + y * math.sin(theta)
-                ir = r_dim * ( 1.0 * r ) / r_max
-                hough_space[int(ir),itheta] = hough_space[int(ir),itheta] + 1
+                ir = r_dim * (1.0 * r) / r_max
+                hough_space[int(ir), itheta] = hough_space[int(ir), itheta] + 1
+
+### chart1
+plt.imshow(hough_space, origin='lower')
+plt.xlim(0, theta_dim)  # we set maximum value on the x-axis of the graph
+plt.ylim(0, r_dim)  # we do the same for y
+
+# this parameters changes the axis signature of the chart
+stepfor_axis_x = 40
+stepfor_axis_y = 20
+
+tick_locs = [i for i in range(0, theta_dim, stepfor_axis_x)] # The list of xtick locations.
+tick_labels = [round((1.0 * i * theta_max) / theta_dim, 1) for i in range(0, theta_dim, stepfor_axis_x)] # The list of xlabel Text objects.
+plt.xticks(tick_locs, tick_labels)
+
+tick_locs = [i for i in range(0, r_dim, stepfor_axis_y)] # The list of ytick locations.
+tick_labels = [round((1.0 * i * r_max) / r_dim, 1) for i in range(0, r_dim, stepfor_axis_y)] # The list of ylabel Text objects.
+plt.yticks(tick_locs, tick_labels)
+
+plt.xlabel(r'Theta')
+plt.ylabel(r'r')
+plt.title('Hough Space')
+
+plt.savefig("hough_space_r_theta.png", bbox_inches='tight')
+
+plt.close()
